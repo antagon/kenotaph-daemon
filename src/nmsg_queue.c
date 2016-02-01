9c5d571e295efa0ff01e1b3dcd707ca7ed2358db
antagon <antagon@codeward.org>
2016-02-01 16:55:24 +0100

    New interface for nmsg_queue.

--- src/nmsg_queue.c
+++ src/nmsg_queue.c
@@ -8,7 +8,7 @@
 #include "nmsg_queue.h"
 
 struct nmsg_node*
-nmsg_node_new (const char *id, const char *type)
+nmsg_node_new (const struct nmsg_text *msg_text)
 {
 	struct nmsg_node *node;
 
@@ -17,27 +17,59 @@ nmsg_node_new (const char *id, const char *type)
 	if ( node == NULL )
 		return NULL;
 
-	// Length: id + semicolon + type + newline
-	node->len = strlen (id) + 1 + strlen (type) + 1;
+	memset (node, 0, sizeof (struct nmsg_node));
 
-	strncpy (node->id, id, NMSG_ID_MAXLEN);
-	node->id[NMSG_ID_MAXLEN] = '\0';
+	snprintf (node->msg, sizeof (node->msg), "%s:%s\n", msg_text->id, msg_text->type);
 
-	strncpy (node->type, type, NMSG_TYPE_MAXLEN);
-	node->type[NMSG_TYPE_MAXLEN] = '\0';
-
-	node->next = NULL;
+	node->len = strlen (msg_text->id) + 1 + strlen (msg_text->type) + 1;
 
 	return node;
+}
+
+int
+nmsg_node_text (const struct nmsg_node *node, struct nmsg_text *msg_text)
+{
+	char *node_buff;
+	size_t i, len, maxlen;
+	int state;
+
+	len = 0;
+	maxlen = NMSG_ID_MAXLEN;
+	node_buff = msg_text->id;
+	state = NMSG_ECON;
+
+	memset (msg_text, 0, sizeof (struct nmsg_text));
+
+	for ( i = 0; i < node->len; i++ ){
+
+		if ( node->msg[i] == ':' ){
+			len = 0;
+			maxlen = NMSG_TYPE_MAXLEN;
+			node_buff = msg_text->type;
+			continue;
+		} else if ( node->msg[i] == '\n' ){
+			state = NMSG_OK;
+			break;
+		}
+
+		if ( len > maxlen )
+			continue;
+
+		node_buff[len++] = node->msg[i];
+	}
+
+	return state;
 }
 
 void
 nmsg_queue_push (struct nmsg_queue *res, struct nmsg_node *node)
 {
 	if ( res->head == NULL ){
+		node->prev = res->head;
 		res->head = node;
 		res->tail = res->head;
 	} else {
+		node->prev = res->tail;
 		res->tail->next = node;
 		res->tail = node;
 	}
@@ -62,10 +94,68 @@ nmsg_queue_serialize (struct nmsg_queue *res, char **buff)
 	bcp = 0;
 
 	for ( node = res->head; node != NULL; node = node->next ){
-		bcp += snprintf ((char*) (*buff + bcp), res->len - bcp, "%s:%s%c", node->id, node->type, '\n');
+		bcp += snprintf ((char*) (*buff + bcp), (res->len + 1) - bcp, "%s", node->msg);
 	}
 
 	return bcp;
+}
+
+ssize_t
+nmsg_queue_unserialize (struct nmsg_queue *res, const char *buff, size_t buff_len)
+{
+	static struct nmsg_node *node = NULL;
+	size_t i;
+
+	//node = NULL;
+
+	for ( i = 0; i < buff_len; i++ ){
+
+		if ( node == NULL ){
+			node = (struct nmsg_node*) malloc (sizeof (struct nmsg_node));
+
+			if ( node == NULL )
+				return -1;
+
+			memset (node, 0, sizeof (struct nmsg_node));
+
+			nmsg_queue_push (res, node);
+		}
+
+		if ( node->len > NMSG_MAXLEN )
+			continue;
+
+		node->msg[node->len++] = buff[i];
+
+		if ( buff[i] == '\n' ){
+			node = NULL;
+			continue;
+		}
+	}
+
+	return i;
+}
+
+void
+nmsg_queue_delete (struct nmsg_queue *res, struct nmsg_node *node)
+{
+	struct nmsg_node *prev_node, *next_node;
+
+	prev_node = node->prev;
+	next_node = node->next;
+
+	free (node);
+
+	if ( prev_node == NULL )
+		res->head = next_node;
+	else
+		prev_node->next = next_node;
+
+	if ( next_node == NULL )
+		res->tail = prev_node;
+	else
+		next_node->prev = prev_node;
+
+	return;
 }
 
 void
--- src/nmsg_queue.h
+++ src/nmsg_queue.h
@@ -4,14 +4,30 @@
 #ifndef _NMSG_QUEUE_H
 #define _NMSG_QUEUE_H
 
-#define NMSG_ID_MAXLEN 127
-#define NMSG_TYPE_MAXLEN 8
+enum
+{
+	NMSG_MAXLEN = 254,
+	NMSG_ID_MAXLEN = 127,
+	NMSG_TYPE_MAXLEN = 8
+};
+
+enum
+{
+	NMSG_OK = 0,
+	NMSG_ECON = 1
+};
+
+struct nmsg_text
+{
+	char id[NMSG_ID_MAXLEN + 1];
+	char type[NMSG_TYPE_MAXLEN + 1];
+};
 
 struct nmsg_node
 {
+	char msg[NMSG_MAXLEN + 1];
 	size_t len;
-	char id[NMSG_ID_MAXLEN + 1];
-	char type[NMSG_TYPE_MAXLEN + 1];
+	struct nmsg_node *prev;
 	struct nmsg_node *next;
 };
 
@@ -22,11 +38,17 @@ struct nmsg_queue
 	size_t len;
 };
 
-extern struct nmsg_node* nmsg_node_new (const char *id, const char *event);
+extern struct nmsg_node* nmsg_node_new (const struct nmsg_text *msg_text);
+
+extern int nmsg_node_text (const struct nmsg_node *node, struct nmsg_text *msg_text);
 
 extern void nmsg_queue_push (struct nmsg_queue *res, struct nmsg_node *node);
 
 extern ssize_t nmsg_queue_serialize (struct nmsg_queue *res, char **buff);
+
+extern ssize_t nmsg_queue_unserialize (struct nmsg_queue *res, const char *buff, size_t buff_len);
+
+extern void nmsg_queue_delete (struct nmsg_queue *res, struct nmsg_node *node);
 
 extern void nmsg_queue_free (struct nmsg_queue *res);
 
